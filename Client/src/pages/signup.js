218548d7a84dfb0d3fafb9f34db7ec1dfc27ad13
DaeHun <skypedanny@naver.com>
2020-03-07 18:39:12 +0900

    [CREATE] Create Signup Form

--- Client/src/pages/signup.js
+++ Client/src/pages/signup.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import AppLayout from "components/AppLayout";
 
+import Head from 'next/head';
+import { Form, Input, Checkbox, Button } from "antd";
+
 const signup = () => {
+  const [id, setId] = useState('');
+  const [nick, setNick] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordCheck, setPasswordCheck] = useState('');
+  const [term, setTerm] = useState(false);
+
+  const onSubmit = () => {};
+  const onChangeId = (e) => {
+      setId(e.target.value);
+  };
+  const onChangeNick = (e) => {
+      setNick(e.target.value);
+  };
+  const onChangePassword = (e) => {
+      setPassword(e.target.value);
+  };
+  const onChangePasswordChk = (e) => {
+      setPasswordCheck(e.target.value);
+  };
+  const onChangeTerm = (e) => {
+      setTerm(e.target.value);
+  };
+
   return (
     <>
       <Head>
@@ -14,7 +40,39 @@ const signup = () => {
       </Head>
 
       <AppLayout>
-        <div>signup</div>
+        <Form onSubmit={onSubmit} style={{ padding: 12, alignItems: "center" }}>
+            <div>
+                <label htmlFor="user-id">ID</label>
+                <br />
+                <Input name="user-id" value={id} required onChange={onChangeId} /> 
+            </div>
+
+            <div>
+                <label htmlFor="user-nick">NickName</label>
+                <br />
+                <Input name="user-nick" value={nick} required onChange={onChangeNick} /> 
+            </div>
+
+            <div>
+                <label htmlFor="user-password">Password</label>
+                <br />
+                <Input name="user-password" type="password" value={password} required onChange={onChangePassword} /> 
+            </div>
+
+            <div>
+                <label htmlFor="user-password-check">Re Password</label>
+                <br />
+                <Input name="user-password-check" type="password" value={passwordCheck} required onChange={onChangePasswordChk} /> 
+            </div>
+
+            <div>
+                <Checkbox name="user-term" value={term} onChange={onChangeTerm}>'이대훈'이 잘생겼다고 생각하면 체크하십시오.</Checkbox>
+            </div>
+
+            <div>
+                <Button type="primary" htmlType="submit">Send</Button>
+            </div>
+        </Form>
       </AppLayout>
     </>
   );
