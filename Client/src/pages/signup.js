a998edc1afda1bd7f1e3a7681656d81d34177726
DaeHun <skypedanny@naver.com>
2020-03-07 17:57:41 +0900

    [CREATE] Create signup page

---
+++ Client/src/pages/signup.js
@@ -0,0 +1,23 @@
+import React from "react";
+import AppLayout from "components/AppLayout";
+
+const signup = () => {
+  return (
+    <>
+      <Head>
+        <title>SNS</title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/antd/4.0.1/antd.css"
+        />
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/antd/4.0.1/antd.min.js" />
+      </Head>
+
+      <AppLayout>
+        <div>signup</div>
+      </AppLayout>
+    </>
+  );
+};
+
+export default signup;
