18775eeecf54243c53b7c936c9a58edb95e46636
DaeHun <skypedanny@naver.com>
2020-03-10 15:43:35 +0900

    [APPLY] Apply antd Grid system

--- Client/src/components/AppLayout.js
+++ Client/src/components/AppLayout.js
@@ -1,22 +1,66 @@
 import React from "react";
-import Link from 'next/link';
+import Link from "next/link";
+import PropTypes from "prop-types";
 
-import { Menu, Input, Button } from "antd";
+import { Menu, Input, Button, Row, Col, Card, Avatar } from "antd";
 
-const AppLayout = ({children}) => {
+const dummy = {
+  nickname: "Danny",
+  Post: [],
+  Followings: [],
+  Followers: [] 
+};
+
+const AppLayout = ({ children }) => {
   return (
     <div>
       <Menu mode="horizontal">
-        <Menu.Item key="home"><Link href="/"><a>SNS</a></Link></Menu.Item>
-        <Menu.Item key="profile"><Link href="/profile"><a>profile</a></Link></Menu.Item>
+        <Menu.Item key="home">
+          <Link href="/">
+            <a>SNS</a>
+          </Link>
+        </Menu.Item>
+        <Menu.Item key="profile">
+          <Link href="/profile">
+            <a>profile</a>
+          </Link>
+        </Menu.Item>
         <Menu.Item key="mail">
-          <Input.Search enterButton style={ { verticalAlign: 'middle' }} />
+          <Input.Search enterButton style={{ verticalAlign: "middle" }} />
         </Menu.Item>
       </Menu>
-      <Link href="/signup"><a><Button>Sign Up</Button></a></Link>
-      {children}
+      <Row>
+        <Col xs={24} md={6}>
+          <Card 
+            actions={[
+              <div key="post">Post<br />{dummy.Post.length}</div>,
+              <div key="follower">follower<br />{dummy.Followers.length}</div>,
+              <div key="following">following<br />{dummy.Followings.length}</div>
+            ]}>
+            <Card.Meta
+              avatar={<Avatar>{dummy.nickname[0]}</Avatar>}
+              title={dummy.nickname}
+            />
+          </Card>
+          <Link href="/signup">
+            <a>
+              <Button>Sign Up</Button>
+            </a>
+          </Link>
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          세 번째
+        </Col>
+      </Row>
     </div>
   );
+};
+
+AppLayout.propTypes = {
+  children: PropTypes.node
 };
 
 export default AppLayout;
