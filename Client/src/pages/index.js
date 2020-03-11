c357e5839183c213881645d3a94607ea35283ae3
DaeHun <skypedanny@naver.com>
2020-03-11 20:40:55 +0900

    [UPDATE] Separate Layout & Connect Components

--- Client/src/pages/index.js
+++ Client/src/pages/index.js
@@ -1,65 +1,30 @@
 import React from "react";
-import { Form, Input, Button, Card, Icon, Avatar } from "antd";
-import { RetweetOutlined, HeartOutlined, MessageFilled, EllipsisOutlined } from '@ant-design/icons';
+
+import PostForm from "components/PostForm";
+import PostCard from "components/PostCard";
 
 const dummy = {
   isLoggedIn: true,
   imagePaths: [],
-  mainPosts: [{
-    User: {
-      id: 1,
-      nickname: 'danny',
-    },
-    content: 'I love you babe',
-    img: 'https://pbs.twimg.com/media/DvbOFSDUUAEj0RU.jpg'
-  }],
-
-}
+  mainPosts: [
+    {
+      User: {
+        id: 1,
+        nickname: "danny"
+      },
+      content: "I love you babe",
+      img: "https://pbs.twimg.com/media/DvbOFSDUUAEj0RU.jpg"
+    }
+  ]
+};
 
 const Home = () => {
   return (
     <>
       <div>
-        {dummy.isLoggedIn && <Form style={{ marginBottom: 30 }}  encType="multipart/form-data">
-          <Input.TextArea maxLength={140} placeholder="What happened?" />
-          <div>
-            <Input type="file" multiple hidden />
-            <Button>Image Upload</Button>
-            <Button type="primary" style={{ float: 'right'}} htmlType="submit">Post</Button>
-          </div>
-          <div>
-            {dummy.imagePaths.map((v, i) => {
-              return ( 
-                <div key={v} style={{ display: 'inline-block' }}>
-                  <img src={'http://localhost:3065/' + v} style={{ width: '200px' }} alt={v} />
-                  <div>
-                    <Button>Delete</Button>
-                  </div>
-                </div>
-              )
-            })}
-          </div>
-        </Form>}
+        {dummy.isLoggedIn && <PostForm />}
         {dummy.mainPosts.map((c) => {
-          return (
-            <Card
-              key={+c.createdAt}
-              cover={c.img && <img alt="example" src={c.img} />}
-              actions={[
-                <RetweetOutlined/>,
-                <HeartOutlined />,
-                <MessageFilled />,
-                <EllipsisOutlined />
-              ]}
-              extra={<Button>Follow</Button>}
-            >
-              <Card.Meta
-                avatar={<Avatar>{c.User.nickname[0]}</Avatar>}
-                title={c.User.nickname}
-                description={c.content}
-              />
-            </Card>
-          )
+          return <PostCard key={c} post={c} />;
         })}
       </div>
     </>
--- Client/src/pages/profile.js
+++ Client/src/pages/profile.js
@@ -1,10 +1,42 @@
 import React from "react";
 
+import { Button, List, Card } from 'antd';
+import { StopOutlined } from '@ant-design/icons';
+import NicknameEditForm from "components/NicknameEditForm";
+
 const profile = () => {
   return (
-    <>
-        <div>profile</div>
-    </>
+    <div>
+      <NicknameEditForm />
+      <List
+        style={{ marginBottom: '20px' }}
+        grid={{ gutter: 4, xs: 2, md: 3 }}
+        size="small"
+        header={<div>Follower List</div>} 
+        loadMore={<Button style={{ width: '100%' }}>More</Button>}
+        bordered
+        dataSource={['Daehun', 'Handsome', 'He is rich']}
+        renderItem={item => (
+          <List.Item style={{ marginTop: '20px' }}>
+            <Card actions={[<StopOutlined key="stop" />]}><Card.Meta description={item} /></Card>
+          </List.Item>
+        )}
+      />
+      <List
+        style={{ marginBottom: '20px' }}
+        grid={{ gutter: 4, xs: 2, md: 3 }}
+        size="small"
+        header={<div>Following List</div>} 
+        loadMore={<Button style={{ width: '100%' }}>More</Button>}
+        bordered
+        dataSource={['Daehun', 'Handsome', 'He is rich']}
+        renderItem={item => (
+          <List.Item style={{ marginTop: '20px' }}>
+            <Card actions={[<StopOutlined key="stop" />]}><Card.Meta description={item} /></Card>
+          </List.Item>
+        )}
+      />
+    </div>
   );
 };
 
